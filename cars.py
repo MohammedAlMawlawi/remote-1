c55a60a89c5d6f0c210a2cde622505ae859f96e4
Mohammed AlMawlawi <145222313+MohammedAlMawlawi@users.noreply.github.com>
2024-03-15 14:19:52 +0300

    Added a new feature to randomly display car brands and also the body shape is now available

--- cars.py
+++ cars.py
@@ -1,5 +1,8 @@
 '''This is a car class
 '''
+from random import randint as rand
+from time import sleep
+
 class car:
     _engine_types: dict = {1 : 'Petrol Engine',
                            2 : 'Hybrid Engine',
@@ -7,10 +10,32 @@ class car:
                            4 : 'Deseil Engine',
                            5 : 'Liquid Gas Engine'
                            }
+    _body_shapes: dict = {1 : 'Sport',
+                          2 : 'Sedan',
+                          3 : 'SUV',
+                          4 : 'Truck',
+                          5 : 'Hatchback'
+                          }
+    _brand_names: list = ['Ferrari', 'Porsche', 'Mercedes', 'BMW']
 
     def __init__(self):
+        print('Welcome to cars game\nYou will create your own car \U0001F600\
+                \nThere are many brands you can choose wither from this list or from your mind')        
+        self._print_brand()
         self._brand = self.set_brand()
         self._engine_type = self._engine_types[self.set_engine_type()]
+        self._body_shape = self._body_shapes[self.set_body_shape()]
+
+    '''This function randomly prints car brands
+    '''
+    def _print_brand(self):
+        random_brand : list = []
+        for i in self._brand_names:
+            random_name = (self._brand_names[rand(1,self._brand_names.__len__() - 1)])
+            if not random_name in random_brand:
+                random_brand.append(random_name)
+                print(random_name)
+                sleep(2)
 
     '''This function returns a str representing the brand name of the car
     '''
@@ -35,6 +60,20 @@ class car:
             engine_type = input('Your selection: ')
         return int(engine_type)
 
+    '''This function returns a number that represents the body shape of the car
+    '''
+    def set_body_shape(self):
+        body_shapes = [f'For {value} = {key}' for key,value in self._body_shapes.items()]
+        msg = 'select the number representing the body shape:\n'
+        for i in body_shapes:
+            print(i)
+        body_shape = input(msg)
+        while not body_shape.isnumeric():
+            for i in body_shapes:
+                print(i)
+            body_shape = input(msg)
+        return int(body_shape)
+
     '''This function returns the brand name of the car
     '''
     def get_brand(self) -> str:
@@ -49,4 +88,5 @@ class car:
 
 if __name__ == "__main__":
     car1 = car()
-    print(f'The details of this car:\n\t-Brand: {car1._brand}\n\t-Engine Type: {car1._engine_type}')
+    print(f'The details of this car:\n\t-Brand: {car1._brand}\n\t-Engine Type: {car1._engine_type}\
+            \n\t-Body Shape: {car1._body_shape}')
